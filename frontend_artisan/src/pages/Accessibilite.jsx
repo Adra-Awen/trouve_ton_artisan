3cfdfa30687026b36d6d4919af3b0e4e03a9cebe
Nolwenn Bauvais <bauvais.n@protonmail.com>
2026-05-26 14:15:05 +0200

    feat: ajout des pages de mentions légales

---
+++ frontend_artisan/src/pages/Accessibilite.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+
+function Accessibilite() {
+    return (
+        <div className="container py-5 text-start">
+            <h2 className="h2-categorie fw-bold mb-4">
+                Accessibilité
+            </h2>
+            <p className="fs-5">
+                Cette page est actuellement en construction.
+            </p>
+        {/*  Bouton retour à l'accueil/ */}
+            <div className="row mt-4">
+                <div className="col text-end">
+                    <Link
+                        to="/"
+                        className="btn btn-outline-primary p-3">
+                        Retour à l'accueil
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Accessibilite;
